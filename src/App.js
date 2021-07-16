19f1671962c034219c34d2f9c075bf071d7ee899
Anton <cminskii5@gmail.com>
2021-07-16 14:06:02 +0300

    add use useSelector, useDispatch, useCallback

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, {
+  useEffect, useState, useSelector, useDispatch, useCallback
+} from 'react';
 import './App.css';
 import * as action from './redux/action';
 import GameBoard from './components/GameBoard';
 import InfoGame from './components/InfoGame';
 import StartMenu from './components/StartMenu';
 
-const App = (props) => {
+const App = () => {
 
-  const {
-    score, failed, timePerMove, resetGameBoard, setStartTime, startTime, setTimerID, timerID, sizeBoard,
-    maxScore, maxLifeCount, isGameOn, setGameStatus, incrementFailCounter
-  } = props;
+  // const {
+  //   score, failed, timePerMove, resetGameBoard, setStartTime, startTime, setTimerID, timerID, sizeBoard,
+  //   maxScore, maxLifeCount, isGameOn, setGameStatus, incrementFailCounter
+  // } = props;
 
-  let sizeContainer = window.innerWidth > window.innerHeight ? window.innerHeight * 0.9 : window.innerWidth * 0.9;
-  let sizeInfo = sizeContainer * 0.2;
-  let widthBoard = sizeContainer * 0.8;
-  let sizeBoardCell = widthBoard / sizeBoard;
-  window.addEventListener('resize', () => {
+  const score = useSelector((state) => state.score);
+  const failed = useSelector((state) => state.failed);
+  const timePerMove = useSelector((state) => state.timePerMove);
+  const startTime = useSelector((state) => state.startTime);
+  const timerID = useSelector((state) => state.timerID);
+  const sizeBoard = useSelector((state) => state.sizeBoard);
+  const maxScore = useSelector((state) => state.maxScore);
+  const maxLifeCount = useSelector((state) => state.maxLifeCount);
+  const isGameOn = useSelector((state) => state.isGameOn);
 
-    sizeContainer = window.innerWidth > window.innerHeight ? window.innerHeight * 0.9 : window.innerWidth * 0.9;
-    sizeInfo = sizeContainer * 0.2;
-    widthBoard = sizeContainer * 0.8;
-    sizeBoardCell = widthBoard / sizeBoard;
+  const dispatch = useDispatch();
+  const incrementFailCounter = useCallback(
+    (value) => dispatch(action.incrementFailCounter(value)),
+    [dispatch]
+  );
+  const resetGameBoard = useCallback(
+    () => dispatch(action.resetGameBoard()),
+    [dispatch]
+  );
+  const setTimerID = useCallback(
+    (value) => dispatch(action.setTimerID(value)),
+    [dispatch]
+  );
+  const setStartTime = useCallback(
+    () => dispatch(action.setStartTime()),
+    [dispatch]
+  );
+  const setGameStatus = useCallback(
+    (value) => dispatch(action.setGameStatus(value)),
+    [dispatch]
+  );
 
-  });
+  const [sizeContainer, setSizeContainer] = useState(
+    window.innerWidth > window.innerHeight ? window.innerHeight * 0.9 : window.innerWidth * 0.9
+  );
+
+  const sizeInfo = sizeContainer * 0.2;
+  const widthBoard = sizeContainer * 0.8;
+  const sizeBoardCell = widthBoard / sizeBoard;
+
+  useEffect(() => {
+
+    window.addEventListener('resize', () => {
+
+      setSizeContainer(window.innerWidth > window.innerHeight ? window.innerHeight * 0.9 : window.innerWidth * 0.9);
+
+    });
+
+  }, []);
 
   if (maxLifeCount <= failed || maxScore <= score) setGameStatus(false);
 
@@ -64,36 +102,36 @@ const App = (props) => {
 
 };
 
-const mapStateToProps = (state) => {
+// const mapStateToProps = (state) => {
 
-  return {
+//   return {
 
-    score: state.score,
-    failed: state.failed,
-    timerID: state.timerID,
-    isGameOn: state.isGameOn,
-    startTime: state.startTime,
-    sizeBoard: state.sizeBoard,
-    maxScore: state.maxScore,
-    timePerMove: state.timePerMove,
-    maxLifeCount: state.maxLifeCount
+//     score: state.score,
+//     failed: state.failed,
+//     timerID: state.timerID,
+//     isGameOn: state.isGameOn,
+//     startTime: state.startTime,
+//     sizeBoard: state.sizeBoard,
+//     maxScore: state.maxScore,
+//     timePerMove: state.timePerMove,
+//     maxLifeCount: state.maxLifeCount
 
-  };
+//   };
 
-};
+// };
 
-const mapDispatchToProps = (dispatch) => {
+// const mapDispatchToProps = (dispatch) => {
 
-  return {
+//   return {
 
-    resetGameBoard: () => dispatch(action.resetGameBoard()),
-    setTimerID: (value) => dispatch(action.setTimerID(value)),
-    setStartTime: () => dispatch(action.setStartTime()),
-    setGameStatus: (value) => dispatch(action.setGameStatus(value)),
-    incrementFailCounter: (value) => dispatch(action.incrementFailCounter(value))
+//     resetGameBoard: () => dispatch(action.resetGameBoard()),
+//     setTimerID: (value) => dispatch(action.setTimerID(value)),
+//     setStartTime: () => dispatch(action.setStartTime()),
+//     setGameStatus: (value) => dispatch(action.setGameStatus(value)),
+//     incrementFailCounter: (value) => dispatch(action.incrementFailCounter(value))
 
-  };
+//   };
 
-};
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
